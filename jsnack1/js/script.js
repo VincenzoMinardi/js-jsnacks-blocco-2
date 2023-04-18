c3eb698d3df111727c23177782c5e93e2e6290ee
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-18 15:00:07 +0200

    add: enter number

---
+++ jsnack1/js/script.js
@@ -0,0 +1,24 @@
+let  numbers;
+let verifiched = false;
+
+const myArr =[];
+
+for  ( let i = 0; i < 6;  i++){
+    num = parseInt(prompt('Digita un numero',));
+    console.log(numbers);
+    
+
+    if(numbers / 2 == 0){
+        console.log('Il numero non è corretto');
+    }
+    
+
+    else if(numbers / 2 == 1){
+        verifiched = true;
+        console.log (myArr.push(numbers));
+    }
+}
+    
+
+
+
