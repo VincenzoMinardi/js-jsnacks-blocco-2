38abdd4e49133ca9b16a727b70c8b8ccc5a48d8d
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-18 16:05:02 +0200

    add: generator name and surname

---
+++ jsnack2/js/script.js
@@ -0,0 +1,28 @@
+for (let i = 0; i >= 6; i++){
+     let arryName = [
+    'Vincenzo',
+     'Lamberto',
+     'Antonio',
+     'Paolo',
+    'Andrea',
+     'Gary',
+] 
+console.log(arryName);
+
+ let arrySurname =[
+     'Minardi',
+     'Pluto',
+     'Pippa',
+     "D'angelo",
+     'Giordano',
+     'Orchestra'
+ ]
+ console.log(arrySurname)
+
+ let ArryList =[]
+
+console.log(ArryList.push,(arryName + arrySurname))
+
+
+}
+
